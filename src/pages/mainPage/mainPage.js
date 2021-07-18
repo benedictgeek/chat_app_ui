a8e5d019873288d6a9db54815427033d2b77a259
olushola <olushola.dauda@algorismng.com>
2021-07-19 00:03:34 +0100

    mainpage in progress

--- src/pages/mainPage/mainPage.js
+++ src/pages/mainPage/mainPage.js
@@ -1,3 +1,23 @@
+import styles from "./mainPage.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUsers } from "@fortawesome/free-solid-svg-icons";
+
 export let MainPage = () => {
-  return <div>Main page here</div>;
+  return (
+    <div>
+      <div className={styles.detailsSection}>
+        <div className={styles.assName}>My Association</div>
+        <span className={styles.assMotto}>moving onwars and forward</span>
+        <div className={styles.membersCount}>
+          <FontAwesomeIcon icon={faUsers} color={"white"} size="1x" />
+          <span className={styles.membersCountNum}>23</span>
+        </div>
+
+        <div className={styles.tabSection}>
+          <div className={styles.activeTab}>CHATS</div>
+          <div className={styles.inActiveTab}>MEMBERS</div>
+        </div>
+      </div>
+    </div>
+  );
 };
